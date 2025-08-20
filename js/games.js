c672dd0a2fb1439deb4e8828121fb81b921403b2
Shyyloww <chamil12377@gmail.com>
2025-08-20 18:35:21 -0400

    Add slope game, player page, and new design

--- js/games.js
+++ js/games.js
@@ -1,13 +1,8 @@
 const games = [
     {
         title: 'Slope',
-        thumbnail: 'assets/images/slope-thumbnail.png', // Create or find a thumbnail for Slope
-        path: 'games/slope/index.html' // <-- This is the important change
+        thumbnail: 'assets/images/slope-thumbnail.png',
+        path: 'games/slope/index.html' // <-- CHECK THIS LINE CAREFULLY
     },
-    // {
-    //     title: 'Run 3',
-    //     thumbnail: 'assets/images/run3-thumbnail.png',
-    //     path: 'games/run3/index.html' // <-- Example for another game
-    // },
     // Add more self-hosted games here...
 ];
