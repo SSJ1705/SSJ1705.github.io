c0cf6d2c6073257ba56dde58b153a7146d469be2
Shyyloww <chamil12377@gmail.com>
2025-08-21 13:27:43 -0400

    asdgsd

--- js/games.js
+++
@@ -1,8 +0,0 @@
-const games = [
-    {
-        title: 'Slope',
-        thumbnail: 'assets/images/slope-thumbnail.png',
-        path: 'games/slope/index.html' // <-- CHECK THIS LINE CAREFULLY
-    },
-    // Add more self-hosted games here...
-];
